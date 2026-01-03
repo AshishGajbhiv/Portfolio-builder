4a2951abc57245af5ace133142eae76ed1563c07
AshishGajbhiv <ashish28gajbhiv@gmail.com>
2026-01-03 22:53:06 +0530

    Fix routing basename for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function AppContent() {
 function App() {
   return (
     <PortfolioProvider>
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <AppContent />
       </BrowserRouter>
     </PortfolioProvider>
